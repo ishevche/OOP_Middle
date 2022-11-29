d16f4550c32a3e6e5e7467ea9e21e5d53f42e7a2
cybermavka <svettacifra@gmail.com>
2022-11-29 20:54:09 +0200

    Company class fields added

--- web_app/src/main/java/com/example/web_app/Company.java
+++ web_app/src/main/java/com/example/web_app/Company.java
@@ -1,4 +1,18 @@
 package com.example.web_app;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
 public class Company {
+    private String domainName;
+    private String name;
+    private String twitter;
+    private String facebook;
+    private String logoLink;
+    private String iconLink;
+    private int emplooyeesMin;
+    private int emplooyeesMax;
+    private String address;
 }
