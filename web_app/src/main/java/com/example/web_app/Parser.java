3c3d7478b55b1c20a57dfd4a4d8d3ec3b89ada74
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2022-12-01 21:16:13 +0200

    Parser for Facebook, Twitter, Wikipedia and name

--- web_app/src/main/java/com/example/web_app/Parser.java
+++ web_app/src/main/java/com/example/web_app/Parser.java
@@ -4,12 +4,47 @@ import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.IOException;
+
 public class Parser {
     private Company company;
 
     @SneakyThrows
     public void getCompanyPageInfo(String query){
         Document doc = Jsoup.connect(query).get();
+    }
 
+    public String getCompanySite(String site, String query) throws IOException {
+        Document doc = Jsoup.connect("https://www.google.com/search?q=site:" + site + " " + query).get();
+        String siteUrl = doc.select("div." + "yuRUbf").select("a").attr("href");
+        return siteUrl;
+    }
+
+    public String getCompanyWikipedia(String query) throws IOException {
+        return getCompanySite("wikipedia.org", query);
+    }
+
+    public String getCompanyFacebook(String query) throws IOException {
+        return getCompanySite("facebook.com", query);
+    }
+
+    public String getCompanyTwitter(String query) throws IOException {
+        return getCompanySite("twitter.com", query);
+    }
+
+    public String getCompanyName(String query) throws IOException {
+        Document doc = Jsoup.connect(new Parser().getCompanyWikipedia(query)).get();
+        return doc.select("span.mw-page-title-main").text();
+    }
+
+    public String getCompanyLogo(String query) throws IOException {
+        Document doc = Jsoup.connect(new Parser().getCompanyWikipedia(query)).get();
+//        String logo = doc.select("span.wikidata-claim").select("img").attr("src");
+//        System.out.println(logo.replace("//", ""));
+//        System.out.println(doc);
+//        System.out.println(doc.select("span.wikidata-claim").select("img").attr("src"));
+//        System.out.println(doc.select("span.wikidata-claim").select("a").attr("href"));
+        return "";
+//        return doc.select("span.mw-page-title-main").text();
     }
 }
