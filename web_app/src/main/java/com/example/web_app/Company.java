843202dc18b82ec10c2f60f536ddfc0913b8ae15
cybermavka <svettacifra@gmail.com>
2022-12-02 12:31:23 +0200

    application.properties for database connection added

--- web_app/src/main/java/com/example/web_app/Company.java
+++ web_app/src/main/java/com/example/web_app/Company.java
@@ -2,10 +2,22 @@ package com.example.web_app;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Entity
+@NoArgsConstructor
 @Getter @Setter @AllArgsConstructor
 public class Company {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String domainName;
     private String name;
     private String twitter;
