406f53b4c1371ae14f2e65d3121a6db0f9c86963
Vladyslav Humennyy <54081501+Tuchis@users.noreply.github.com>
2022-12-02 22:09:22 +0200

    BrandFetch connected

--- web_app/src/main/java/com/example/web_app/Parser.java
+++ web_app/src/main/java/com/example/web_app/Parser.java
@@ -19,11 +19,14 @@ public class Parser {
     private boolean wikiParsed = false;
     private Document wiki;
     private String wikiLink;
+    private BrandFetch brandFetch;
+    private JSONObject PDAJson;
     private final Company company = new Company();
 
     public Parser(String domain){
         this.domain = domain;
         this.company.setDomainName(domain);
+        this.brandFetch = new BrandFetch(domain);
     }
 
     private String getCompanySite(String site) throws IOException {
@@ -36,11 +39,21 @@ public class Parser {
     }
 
     void getCompanyFacebook() throws IOException {
-        this.company.setFacebook(getCompanySite("facebook.com"));
+        if (brandFetch.getFacebook_url() != null){
+            this.company.setFacebook(brandFetch.getFacebook_url());
+        }
+        else {
+            this.company.setFacebook(getCompanySite("facebook.com"));
+        }
     }
 
     void getCompanyTwitter() throws IOException {
-        this.company.setTwitter(getCompanySite("twitter.com"));
+        if (brandFetch.getTwitter_url() != null){
+            this.company.setTwitter(brandFetch.getTwitter_url());
+        }
+        else {
+            this.company.setTwitter(getCompanySite("twitter.com"));
+        }
     }
 
     void getCompanyName() throws IOException {
@@ -53,45 +66,47 @@ public class Parser {
     }
 
     void getCompanyLogo() throws IOException {
-        if (!wikiParsed){
-            this.wikiLink = getCompanyWikipedia();
-            this.wiki = Jsoup.connect(this.wikiLink).get();
-            wikiParsed = true;
+        if (brandFetch.getLogo_url() != null){
+            this.company.setLogoLink(brandFetch.getLogo_url());
         }
-        String logo = wiki.select("table.infobox")
-                .select("img").attr("src");
-        this.company.setLogoLink(logo.replace("//", ""));
+        else {
+            if (!wikiParsed) {
+                this.wikiLink = getCompanyWikipedia();
+                this.wiki = Jsoup.connect(this.wikiLink).get();
+                wikiParsed = true;
+            }
+            String logo = wiki.select("table.infobox")
+                    .select("img").attr("src");
+            this.company.setLogoLink(logo.replace("//", ""));
+        }
     }
 
     void getCompanyIcon() throws IOException {
-        if (!wikiParsed){
-            this.wikiLink = getCompanyWikipedia();
-            this.wiki = Jsoup.connect(this.wikiLink).get();
-            wikiParsed = true;
+        if (brandFetch.getIcon_url() != null){
+            this.company.setIconLink(brandFetch.getIcon_url());
         }
-        String logo = wiki.select("table.infobox").select("img").attr("src");
-        this.company.setIconLink(logo.replace("//", ""));
+        else {
+            if (!wikiParsed) {
+                this.wikiLink = getCompanyWikipedia();
+                this.wiki = Jsoup.connect(this.wikiLink).get();
+                wikiParsed = true;
+            }
+            String logo = wiki.select("table.infobox")
+                    .select("img").attr("src");
+            this.company.setIconLink(logo.replace("//", ""));
+        }
     }
 
     void getCompanyEmployees(){
-        this.company.setEmplooyees("100-500");
+        this.company.setEmplooyees(PDAJson.getJSONArray("data").getJSONObject(0).getString("employee_count"));
     }
 
     void getCompanyAddress() throws IOException {
-        String API_KEY = System.getenv("PDL_API_KEY");
-        String query = URLEncoder.encode("SELECT NAME FROM COMPANY WHERE WEBSITE='" + this.domain + "'", StandardCharsets.UTF_8);
-        URL url = new URL("https://api.peopledatalabs.com/v5/company/search?sql=" + query);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("X-Api-Key", API_KEY);
-        connection.connect();
-        String text = new Scanner(connection.getInputStream()).useDelimiter("\\Z").next();
-        JSONObject jsonObject = new JSONObject(text);
         ArrayList<String> JSONNames = new ArrayList<String>(Arrays.asList("continent", "country", "street_address", "address_line_2", "geo"));
         ArrayList<String> output = new ArrayList<String>();
         for (int i = 0; i < JSONNames.size(); i++){
-            if (!jsonObject.getJSONArray("data").getJSONObject(0).getJSONObject("location").isNull(JSONNames.get(i))){
-                output.add(jsonObject.getJSONArray("data").getJSONObject(0).getJSONObject("location").getString(JSONNames.get(i)));
+            if (!PDAJson.getJSONArray("data").getJSONObject(0).getJSONObject("location").isNull(JSONNames.get(i))){
+                output.add(PDAJson.getJSONArray("data").getJSONObject(0).getJSONObject("location").getString(JSONNames.get(i)));
             }
             if (i == 1){
                 output = new ArrayList<>(Arrays.asList(String.join(", ", output)));
@@ -100,8 +115,22 @@ public class Parser {
         this.company.setAddress(String.join("\n", output));
     }
 
+    private void fetchPDA() throws IOException {
+        String API_KEY = System.getenv("PDL_API_KEY");
+        String query = URLEncoder.encode("SELECT NAME FROM COMPANY WHERE WEBSITE='" + this.domain + "'", StandardCharsets.UTF_8);
+        URL url = new URL("https://api.peopledatalabs.com/v5/company/search?sql=" + query);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("X-Api-Key", API_KEY);
+        connection.connect();
+        String text = new Scanner(connection.getInputStream()).useDelimiter("\\Z").next();
+        this.PDAJson = new JSONObject(text);
+    }
+
     public void fetchInformation() {
         try {
+            brandFetch.fetchInfo();
+            fetchPDA();
             getCompanyName();
             getCompanyFacebook();
             getCompanyTwitter();
